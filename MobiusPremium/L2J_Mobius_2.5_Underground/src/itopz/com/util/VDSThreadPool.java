95c99f35b7c972ac9bad8743a01740c059376471
nightw0lv <dotaddict69@hotmail.gr>
2021-11-01 01:14:52 +0200

    Mobius Premium Underground 2.5 Update
    Updating version
    Merging older version info
    Setting project JDK 17
    Updating Patch.diff

--- MobiusPremium/L2J_Mobius_2.5_Underground/src/itopz/com/util/VDSThreadPool.java
+++ MobiusPremium/L2J_Mobius_2.5_Underground/src/itopz/com/util/VDSThreadPool.java
@@ -31,7 +31,7 @@ import java.util.concurrent.*;
  * <p>
  * Vote Donation System
  * Script website: https://itopz.com/
- * Script version: 1.3
+ * Script version: 1.4
  * Pack Support: Mobius Premium 2.5 Underground
  * <p>
  * Personal Donate Panels: https://www.denart-designs.com/
