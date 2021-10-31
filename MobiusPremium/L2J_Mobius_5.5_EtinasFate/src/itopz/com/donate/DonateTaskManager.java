3240392add168e3bffd047e52e37d830816f0c99
nightw0lv <dotaddict69@hotmail.gr>
2021-11-01 01:21:03 +0200

    Mobius Premium Etinas Fate 5.5 Update
    Updating version
    Merging older version info
    Setting project JDK 17
    Updating Patch.diff

--- MobiusPremium/L2J_Mobius_5.5_EtinasFate/src/itopz/com/donate/DonateTaskManager.java
+++ MobiusPremium/L2J_Mobius_5.5_EtinasFate/src/itopz/com/donate/DonateTaskManager.java
@@ -46,7 +46,7 @@ import java.util.Optional;
  * <p>
  * Vote Donation System
  * Script website: https://itopz.com/
- * Script version: 1.3
+ * Script version: 1.4
  * Pack Support: Mobius Premium 5.5 Etinas Fate
  * <p>
  * Personal Donate Panels: https://www.denart-designs.com/
