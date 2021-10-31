005901f9c7e28a2630a2bfd61445a03008f0f92b
nightw0lv <dotaddict69@hotmail.gr>
2021-11-01 01:58:49 +0200

    Mobius Premium Epilogue 2.4 Update
    Updating version
    Merging older version info
    Setting project JDK 17
    Updating Patch.diff

--- MobiusPremium/L2J_Mobius_CT_2.4_Epilogue/src/itopz/com/util/Utilities.java
+++ MobiusPremium/L2J_Mobius_CT_2.4_Epilogue/src/itopz/com/util/Utilities.java
@@ -54,7 +54,7 @@ import java.util.Locale;
  * <p>
  * Vote Donation System
  * Script website: https://itopz.com/
- * Script version: 1.3
+ * Script version: 1.4
  * Pack Support: Mobius Premium CT 2.4 Epilogue
  * <p>
  * Personal Donate Panels: https://www.denart-designs.com/
