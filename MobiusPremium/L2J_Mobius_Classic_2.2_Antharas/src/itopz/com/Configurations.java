52f5eab1248571eba00e754b89b7bc55cbf81104
nightw0lv <dotaddict69@hotmail.gr>
2021-11-01 01:53:09 +0200

    Mobius Premium Antharas 2.2 Update
    Updating version
    Merging older version info
    Setting project JDK 17
    Updating Patch.diff

--- MobiusPremium/L2J_Mobius_Classic_2.2_Antharas/src/itopz/com/Configurations.java
+++ MobiusPremium/L2J_Mobius_Classic_2.2_Antharas/src/itopz/com/Configurations.java
@@ -37,7 +37,7 @@ import java.util.*;
  * <p>
  * Vote Donation System
  * Script website: https://itopz.com/
- * Script version: 1.3
+ * Script version: 1.4
  * Pack Support: Mobius Premium 2.2 Antharas
  * <p>
  * Personal Donate Panels: https://www.denart-designs.com/
