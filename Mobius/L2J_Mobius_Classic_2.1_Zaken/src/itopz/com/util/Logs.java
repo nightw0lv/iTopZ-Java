459c3cfdc49e439aa9b55a242cb6ba943c5eacb7
nightw0lv <dotaddict69@hotmail.gr>
2021-11-01 00:53:35 +0200

    Mobius Zaken 2.1 Update
    Updating version
    Merging older version info
    Setting project JDK 14

--- Mobius/L2J_Mobius_Classic_2.1_Zaken/src/itopz/com/util/Logs.java
+++ Mobius/L2J_Mobius_Classic_2.1_Zaken/src/itopz/com/util/Logs.java
@@ -32,7 +32,7 @@ import java.util.logging.Logger;
  * <p>
  * Vote Donation System
  * Script website: https://itopz.com/
- * Script version: 1.3
+ * Script version: 1.4
  * Pack Support: Mobius 2.1 Zaken
  * <p>
  * Personal Donate Panels: https://www.denart-designs.com/
