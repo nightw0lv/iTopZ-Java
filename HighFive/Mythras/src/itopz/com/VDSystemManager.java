6604e9de8bd6ee0cf9618a3f84cbbb4c1ca5ff7f
nightw0lv <dotaddict69@hotmail.gr>
2021-10-31 21:44:24 +0200

    Mythras Update
    Updating version
    Merging older version info

--- HighFive/Mythras/src/itopz/com/VDSystemManager.java
+++ HighFive/Mythras/src/itopz/com/VDSystemManager.java
@@ -34,7 +34,7 @@ import l2f.gameserver.GameServer;
  * <p>
  * Vote Donation System
  * Script website: https://itopz.com/
- * Script version: 1.3
+ * Script version: 1.4
  * Pack Support: Mythras
  * <p>
  * Personal Donate Panels: https://www.denart-designs.com/
--- HighFive/Mythras/src/itopz/com/donate/DonateTaskManager.java
+++ HighFive/Mythras/src/itopz/com/donate/DonateTaskManager.java
@@ -46,7 +46,7 @@ import java.util.Optional;
  * <p>
  * Vote Donation System
  * Script website: https://itopz.com/
- * Script version: 1.3
+ * Script version: 1.4
  * Pack Support: Mythras
  * <p>
  * Personal Donate Panels: https://www.denart-designs.com/
