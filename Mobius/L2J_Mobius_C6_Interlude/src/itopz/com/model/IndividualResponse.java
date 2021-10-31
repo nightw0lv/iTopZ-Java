9f55f1b6b066b5b02abb48a77feb0af30d462706
nightw0lv <dotaddict69@hotmail.gr>
2021-11-01 00:50:00 +0200

    Mobius Interlude C6 Update
    Updating version
    Merging older version info
    Setting project JDK 14

--- Mobius/L2J_Mobius_C6_Interlude/src/itopz/com/model/IndividualResponse.java
+++ Mobius/L2J_Mobius_C6_Interlude/src/itopz/com/model/IndividualResponse.java
@@ -35,7 +35,7 @@ import itopz.com.vote.VDSystem;
  * <p>
  * Vote Donation System
  * Script website: https://itopz.com/
- * Script version: 1.3
+ * Script version: 1.4
  * Pack Support: Mobius C6 Interlude
  * <p>
  * Personal Donate Panels: https://www.denart-designs.com/
