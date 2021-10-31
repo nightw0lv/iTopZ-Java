a21071b260bf637b6f739a02b468ef4f4b51b511
nightw0lv <dotaddict69@hotmail.gr>
2021-11-01 01:56:38 +0200

    Mobius Premium The Kamael 3.0 Update
    Updating version
    Merging older version info
    Setting project JDK 17
    Updating Patch.diff

--- MobiusPremium/L2J_Mobius_Classic_3.0_TheKamael/src/itopz/com/Configurations.java
+++ MobiusPremium/L2J_Mobius_Classic_3.0_TheKamael/src/itopz/com/Configurations.java
@@ -37,7 +37,7 @@ import java.util.*;
  * <p>
  * Vote Donation System
  * Script website: https://itopz.com/
- * Script version: 1.3
+ * Script version: 1.4
  * Pack Support: Mobius Premium 3.0 The Kamael
  * <p>
  * Personal Donate Panels: https://www.denart-designs.com/
--- MobiusPremium/L2J_Mobius_Classic_3.0_TheKamael/src/itopz/com/util/Random.java
+++ MobiusPremium/L2J_Mobius_Classic_3.0_TheKamael/src/itopz/com/util/Random.java
@@ -31,7 +31,7 @@ import java.util.concurrent.ThreadLocalRandom;
  * <p>
  * Vote Donation System
  * Script website: https://itopz.com/
- * Script version: 1.3
+ * Script version: 1.4
  * Pack Support: Mobius Premium 3.0 The Kamael
  * <p>
  * Personal Donate Panels: https://www.denart-designs.com/
