9cbed76b8c38e062408c300a8a173f31304ed706
nightw0lv <dotaddict69@hotmail.gr>
2021-10-31 21:52:54 +0200

    ScriptsZaken Update
    Updating version
    Merging older version info

--- HighFive/ScriptsZaken/src/itopz/com/command/VoteCMD.java
+++ HighFive/ScriptsZaken/src/itopz/com/command/VoteCMD.java
@@ -49,7 +49,7 @@ import java.util.Objects;
  * <p>
  * Vote Donation System
  * Script website: https://itopz.com/
- * Script version: 1.3
+ * Script version: 1.4
  * Pack Support: L2Scripts Classic: Saviors (Zaken) [26360]
  * <p>
  * Personal Donate Panels: https://www.denart-designs.com/
--- HighFive/ScriptsZaken/src/itopz/com/vote/VDSystem.java
+++ HighFive/ScriptsZaken/src/itopz/com/vote/VDSystem.java
@@ -38,7 +38,7 @@ import l2s.gameserver.handler.voicecommands.VoicedCommandHandler;
  * <p>
  * Vote Donation System
  * Script website: https://itopz.com/
- * Script version: 1.3
+ * Script version: 1.4
  * Pack Support: L2Scripts Classic: Saviors (Zaken) [26360]
  * <p>
  * Personal Donate Panels: https://www.denart-designs.com/
